aab1cce93ff94504edee8f663b0b9272165eb8b0
LucaPalermo <luca.palermo98@gmail.com>
2020-04-09 15:18:15 +0200

    added odin

---
+++ DialogueSystem/DialogueSystem/Assets/Editor/CustomDrawer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Dialogue))]
+[CanEditMultipleObjects]
+public class CustomDrawer : Editor
+{
+
+	private void OnEnable()
+	{
+		
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+	}
+}
